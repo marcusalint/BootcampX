6529cb20beeb69e0c0c6fd03beb53be9b9ac34c4
salar taeb <salartaeb10@gmail.com>
2020-11-26 06:08:41 +0000

    commit

---
+++ 1_queries/1queries.sql
@@ -0,0 +1,17 @@
+SELECT id, name
+FROM students 
+WHERE cohort_id = 1
+ORDER BY name;
+
+SELECT count(id) 
+FROM students
+WHERE cohort_id IN (1,2,3);
+
+SELECT name, id, cohort_id 
+FROM students
+WHERE email IS NULL
+OR phone IS NULL;
+
+SELECT name, email, phone 
+FROM students
+WHERE github IS NULL;
